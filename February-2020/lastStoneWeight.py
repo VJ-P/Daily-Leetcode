0c36736b7c7f72f826b23028844ac34cabee2b6a
Vijay Patel <108.vijayp@gmail.com>
2021-02-05 12:41:01 -0500

    Add Solution for Last Stone Weight

---
+++ February-2020/lastStoneWeight.py
@@ -0,0 +1,10 @@
+import heapq
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        for i in range(len(stones)):
+            stones[i] = -stones[i]
+        heapq.heapify(stones)
+        while len(stones) > 1:
+            heapq.heappush(stones, -abs(heapq.heappop(stones) - heapq.heappop(stones)))
+        
+        return -stones[0]

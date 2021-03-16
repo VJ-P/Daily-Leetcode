3e919632b3b63a0c1f8446140b38b8972a1caa4a
Vijay Patel <108.vijayp@gmail.com>
2021-03-15 20:41:56 -0400

    Add Solution for Nested List Weight Sum

---
+++ March-2020/depthSum.py
@@ -0,0 +1,17 @@
+# Given a nested list of integers, return the sum of all integers in the list weighted by their depth. Each element is either an integer, 
+# or a list -- whose elements may also be integers or other lists.
+
+def depthSum(nestedList):
+    sumDepth = 0
+    def dfs(array, depth):
+        nonlocal sumDepth
+        for item in array:
+            if type(item) == int:
+                sumDepth += item * depth
+            else:
+                dfs(item, depth + 1)
+    dfs(nestedList, 1)
+    return sumDepth
+
+# print(depthSum([[1,1],2,[1,1]]))
+# print(depthSum([1,[4,[6]]]))
